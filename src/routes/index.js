eb14b9c456ee25ce69e77dbf4f6af4991546db84
Tài Lê <letai97md@gmail.com>
2023-04-24 21:15:48 +0700

    Design teacher page and handle route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import Register from "../pages/Register";
 import Teacher from "../pages/Teacher";
 
 const publicRoutes = [
-    {path: "/", component: Home},
+    {path: "/Study-With-Me", component: Home},
     {path: "/khoa-hoc-truc-tuyen/:id", component: DetailCourse},
     {path: "/giao-vien", component: Teacher},
     {path: "/loginv2", component: Login},
