4b8c69bb4ac13a5803232a7baa1077daa01209f6
Tài Lê <letai97md@gmail.com>
2023-04-24 21:23:13 +0700

    Design teacher page and handle route

--- src/components/Layout/components/Header/sidebar.jsx
+++ src/components/Layout/components/Header/sidebar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
 
 export const Sidebar = ({ name, ...props }) => {
--- src/components/Slide/rightContent.jsx
+++ src/components/Slide/rightContent.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import chungnhan from "../../assets/Images/Banner/chungnhan-04.png"
+import chungnhan from "../../assets/Images/Banner/chungnhan-04.png";
 export const RightContent = () => {
   return (
     <div className="header-right-content">
       <div className="box-img">
-        <img
-          src={chungnhan}
-          alt=""
-        />
+        <img src={chungnhan} alt="" />
       </div>
       <div className="btn-dowload bg-btn">
         <a
           className="gg-analytic"
           data-event="download-app"
-          target="_blank"
+          target="blank"
           href="#"
         >
           TẢI ỨNG DỤNG HOCMAI
