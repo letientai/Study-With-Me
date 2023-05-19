d9af9c9dd34ea308a4dcc366ad05d14366087c14
haulamle <haulamle@gmail.com>
2023-05-19 20:26:06 +0700

    them chuc nang forgot pÃassword

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,12 +8,12 @@ import Courses from "../pages/Courses";
 import {ActorCourses, ActorLesson,ActorChapter,ListChapterAndLesson} from '../pages/ActorCourses' 
 import DetailTeacher from "../pages/DetailTeacher";
 import ActorStudents from "../pages/ActorStudents";
+import Forgot from "../pages/Forgot";
 import { ChangPassword } from "../pages/ChangPassword";
 import { ActorCoursesAdd} from "../pages/ActorCourses";
 import { Lesson } from "../pages/Lesson";
 import { Checkout } from "../pages/Checkout";
 import { MyCourses } from "../pages/MyCourses";
-
 const publicRoutes = [
     { path: "/Study-With-Me", component: Home },
     { path: "/khoa-hoc-truc-tuyen/:id", component: DetailCourse },
@@ -23,12 +23,13 @@ const publicRoutes = [
     { path: "/doi-mat-khau", component: ChangPassword },
     { path: "/dang-nhap", component: Login },
     { path: "/dang-ky", component: Register },
+    { path: "/forgot", component: Forgot },
     { path: "/course", component: Courses },
     { path: "/khoa-hoc/:idCourse/bai-hoc/:idLesson", component: Lesson },
     { path: "/khoa-hoc-cua-toi", component: MyCourses },
     { path: "/add-lesson", component: ActorLesson },
     { path: "/thanh-toan/:idCourse", component: Checkout },
-    { path: "/search/:type", component: Courses },
+    { path: "/search/:type?", component: Courses },
     { path: "/actor-courses", component: ActorCourses },
     { path: "/actor-courses/:id", component: ActorCoursesAdd},
     { path: "/actor-chapter/:id", component:ActorChapter },
